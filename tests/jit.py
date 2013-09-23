8e7f16cc0edd179108a5ab97a892bbcacd183732
Tobias Pape <tobias@netshed.de>
2013-09-23 17:40:22 +0200

    fix jit test and add recursive (does not produce trace yet)

--- tests/jit.py
+++ tests/jit.py
@@ -22,23 +22,59 @@ cp = py.path.local(__file__).dirpath().dirpath().join("Smalltalk").strpath
 
 class TestLLtype(LLJitMixin):
 
-    def test_inc(self):
+
+    def _compile(self, source, start):
         u = Universe()
         u.setup_classpath(cp)
         u._initialize_object_system()
         interp = u.get_interpreter()
         b = u._create_bootstrap_method()
-        f = interp.push_new_frame(b)
-        src = "C_0 = ( run = ( | tmp | tmp := 1. 1000000 timesRepeat: [ tmp := tmp + 1 ]. ^tmp ))"
-        cls = sourcecode_compiler.compile_class_from_string(src, None, u)
-        run = cls.lookup_invokable(u.symbol_for("run"))
-        
+        frame = interp.push_new_frame(b)
+        cls = sourcecode_compiler.compile_class_from_string(source, None, u)
+        obj = u.new_instance(cls)
+        frame.push(obj)
+        invokable = cls.lookup_invokable(u.symbol_for(start))
+        return (u, frame, invokable)
+
+    def test_inc(self):
+        universe, frame, invokable = self._compile(
+            """
+            C_0 = (
+                run = ( | tmp |
+                        tmp := 1.
+                        10000 timesRepeat: [
+                          tmp := tmp + 1 ].
+                        ^tmp
+                )
+            )
+            """, "run")
         def interp_w():
             try:
-                run.invoke(f, interp)
-                interp.start()
+                universe.start(frame, invokable)
             except Exit as e:
                 return e.code
             return -1
 
-        self.meta_interp(interp_w, [], listcomp=True, listops=True, backendopt=True)
+        self.meta_interp(interp_w, [],
+                         listcomp=True, listops=True, backendopt=True)
+
+    def test_rec(self):
+        universe, frame, invokable = self._compile(
+            """
+            C_1 = (
+                count: n = ( ^ (n > 0)
+                                 ifTrue: [self count: n - 1]
+                                 ifFalse: [n]
+                )
+                run = ( ^ self count: 100000 )
+            )
+            """, "run")
+        def interp_w():
+            try:
+                universe.start(frame, invokable)
+            except Exit as e:
+                return e.code
+            return -1
+
+        self.meta_interp(interp_w, [],
+                         listcomp=True, listops=True, backendopt=True)
