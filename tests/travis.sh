c38416bc4c0c93e8c09d3f605082eb1a985f27fb
Tobias Pape <tobias@netshed.de>
2013-09-23 14:20:31 +0200

    move travis actual execution to tests/travis.sh

---
+++ tests/travis.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -x
+
+pypy/pytest
+nosetests
+
+if [ "$RUN" = "hosted" ]; then
+  ./som.sh -cp Smalltalk TestSuite/TestHarness.som;
+elif [ "$RUN" = "translated" ]; then
+  pypy/rpython/bin/rpython -O$OPTIMIZE --batch src/targetsomstandalone.py
+  ./som -cp Smalltalk TestSuite/TestHarness.som
+else
+  exit 2
+fi
